a74b812786ca59803143b075af5e240996da3be3
Khiet Phan <khietphan@outlook.com>
2017-06-01 16:21:13 +0700

    Move to new DotNetCore Structure

--- src/app/Component/Dashboard/dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'dashboard',
-    templateUrl: './dashboard.html',
-    styleUrls: ['./dashboard.css']
-})
-export class DashboardComponent {
-}
